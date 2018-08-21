7fc1217fd46f069eb0f0ffbceff8df3dc6464209
lenovo <lenovo@LAPTOP-11CT66RH>
2018-08-20 17:58:28 -0700

    secondway

--- src/com/scp/jdbc/demo/JdbcDemo.java
+++ src/com/scp/jdbc/demo/JdbcDemo.java
@@ -12,7 +12,7 @@ public class JdbcDemo {
 
 	public static void main(String[] args) //throws ClassNotFoundException, SQLException {
 	{
-		try{
+		try{System.out.println("in try");
 		
        Class.forName("com.mysql.jdbc.Driver");
        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql","root","root");
